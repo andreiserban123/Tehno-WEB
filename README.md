1472dc117b60cc63f7f5b7c0d8c44f13ad6ad6d6
Serban Timofte <serbantimofte@Serbans-MacBook-Pro.local>
2025-02-12 13:34:28 +0200

    Adding CRUD comments endpoints

---
+++ README.md
@@ -0,0 +1,5 @@
+# Implemantari in acest branch
+
+- CRUD pentru comments
+
+- In task details avem o sectine pentru comments
---
+++ server/models/comment.mjs
@@ -0,0 +1,36 @@
+export default (sequelize, DataTypes) => {
+  return sequelize.define("comment", {
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    taskId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "tasks",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+  });
+};
